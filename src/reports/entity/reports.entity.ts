f0e576cf76251831ad2236d9057d14c09be4ebc5
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-01-12 09:56:08 -0100

    updated the reports entity

--- src/reports/entity/reports.entity.ts
+++ src/reports/entity/reports.entity.ts
@@ -9,4 +9,21 @@ export class Reports {
     @Column()
     price: number;
 
+    @Column()
+    make: string;
+
+    @Column()
+    model: string;
+
+    @Column()
+    year: number;
+
+    @Column()
+    longitude: number;
+
+    @Column()
+    latitude: number;
+
+    @Column()
+    mileage: number;
 }
