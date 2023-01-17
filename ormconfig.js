66095e7756eb6d7d3872eda63f3dfe083877fb7d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-01-17 10:52:34 -0100

    fixed configs for production. deployed app in heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,9 @@
 const dbConfig = {
   synchronize: false,
   migrations: ['migrations/*.js'],
-  
+  cli: {
+    migrationsDir: 'migrations'
+  }
 };
 
 switch (process.env.NODE_ENV) {
@@ -21,6 +23,15 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsRun: true,
+      entities: ['**/*.entity.js'],
+      ssl: {
+        rejectUnauthorized: false
+      }
+    })
     break;
   default:
     throw new Error('unknown environment');
